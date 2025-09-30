868a2d1dde1af2a88f2bd3af13ea53ef6aa41e13
sulig312 <sulig321@gmail.com>
2025-09-30 13:10:55 -0400

    updated files for prod

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,23 @@
+#IAM Execution Role
+
+resource "aws_iam_role" "ecs_task_execution" {            #IAM
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {        #IAM
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,63 +4,26 @@ variable "region" {
   
 }
 
-variable "cidr-vpc" {
+
+
+variable "aws_log_region" {
     type = string
-    default = "10.0.0.0/16"
+    default = "us-east-2"
   
 }
 
-variable "cidr-public1" {
-    type = string
-    default = "10.0.1.0/24"
-  
-}
 
-variable "cidr-private1" {
-    type = string
-    default = "10.0.2.0/24"
-  
-}
 
-variable "cidr-public2" {
-    type = string
-    default = "10.0.5.0/24"
-  
-}
 
-variable "cidr-private2" {
-    type = string
-    default = "10.0.4.0/24"
-  
-}
 
-variable "domain-name" {
-    type = string
-    default = "*.sulig.click" #why? in case i need to use like app.sulig.click etc
+# variable "task_def_cpu" {
+#     type = string
+#     default = "256"
   
-}
+# }
 
-variable "acm-vald-method" {
-    type = string
-    default = "DNS"
+# variable "task_def_memory" {
+#     type = string
+#     default = "512"
   
-}
-
-variable "load-bal-type" {
-    type = string
-    default = "application"
-  
-}
-
-variable "tg1-port" {
-    type = string
-    default = "3000"
-  
-}
-
-variable "tg1-protocol1" {
-    type = string
-    default = "HTTP"
-  
-}
-
+# }
